7742e441823b2c360888866e63590cb8f8192579
Yogesh Shukla <Yogesh1#0>
2019-08-31 00:34:47 +0530

    First commit

---
+++ src/test/java/starter/navigation/DuckDuckGoHomePage.java
@@ -0,0 +1,15 @@
+package starter.navigation;
+
+import net.serenitybdd.core.pages.PageObject;
+import net.thucydides.core.annotations.DefaultUrl;
+
+@DefaultUrl("https://www.google.com")
+public class DuckDuckGoHomePage extends PageObject {
+
+    public void me(){
+
+    }
+
+
+
+}
