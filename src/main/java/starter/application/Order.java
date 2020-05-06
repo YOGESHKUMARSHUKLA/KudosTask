829b9efea47420adc5cbaf11757b6f10fed18b47
Yogesh Shukla <Yogesh1#0>
2020-05-06 21:32:54 +0530

    Uploaded task Code

--- src/main/java/starter/application/Order.java
+++
@@ -1,7 +0,0 @@
-package starter.application;
-
-public class Order {
-    private String orderNo;
-    public Order(){}
-    public Order(String orderNo){this.orderNo =orderNo;}
-}
--- src/main/java/starter/generic/CsvMapper.java
+++
@@ -1,6 +0,0 @@
-package starter.generic;
-
-public class CsvMapper extends InputTemplate {
-    private static final String CSV_LOCATION = "/template/csv/";
-    public CsvMapper(String path){super(CSV_LOCATION+path);}
-}
